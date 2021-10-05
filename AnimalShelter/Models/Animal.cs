11d0199fb3d17551dad73b78f921a65974c5ed6b
Connor Hansen <chansen13@georgefox.edu>
2021-10-05 13:53:33 -0700

    Add ability to add animals to database, view details on click.
    Co-authored-by: Chynna Lew <chynnalew@yahoo.com>

--- AnimalShelter/Models/Animal.cs
+++ AnimalShelter/Models/Animal.cs
@@ -6,22 +6,13 @@ namespace AnimalShelter.Models
   public class Animal
   {
     public int AnimalId { get; set; }
-    public Types ThisTypes { get; set; }
+    public string ThisTypes { get; set; }
     public string Name { get; set; }
-    public Gender Gender { get; set; }
+    public string Gender { get; set; }
     public DateTime DateAdmitted { get; set; }
     public string Breed { get; set; }
   }
-  public enum Types
-  {
-    Dog,
-    Cat
-  }
-  public enum Gender
-  {
-    Male,
-    Female
-  }
+
 }
 
 //     public TeaType SelectTeaType { get; set; }
--- AnimalShelter/Models/AnimalShelterContext.cs
+++ AnimalShelter/Models/AnimalShelterContext.cs
@@ -9,3 +9,9 @@ namespace AnimalShelter.Models
     public AnimalShelterContext(DbContextOptions options) : base(options) { }
   }
 }
+
+// MySqlException: Unknown column 'a.ThisTypes' in 'field list'
+// MySqlConnector.Core.ResultSet.ReadResultSetHeaderAsync(IOBehavior ioBehavior) in ResultSet.cs, line 49
+
+// MySqlException: Unknown column 'a.ThisTypes' in 'field list'
+// MySqlConnector.MySqlDataReader.ActivateResultSet(CancellationToken cancellationToken) in MySqlDataReader.cs, line 131
